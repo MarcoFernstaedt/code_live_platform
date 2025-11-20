8f83f02007ae2c68354a4e3290a8a001235b8e53
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-11-19 19:12:26 -0700

    chore: configure env keys and add minimal Clerk auth UI

--- server/src/lib/env.ts
+++ server/src/lib/env.ts
@@ -4,4 +4,5 @@ export const ENV = {
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
     NODE_ENV: process.env.NODE_ENV,
-}
+    DATABASE_URI: process.env.DATABASE_URI,
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,29 @@
 import express from 'express';
-import router from './routes/index.ts'; 
+import type { Request, Response } from 'express';
+import router from './routes/index.ts';
 import { ENV } from './lib/env.ts';
+import connectDB from './lib/db.ts';
 import path from 'path';
 
 const app = express();
-const PORT = ENV.PORT;
+const PORT = ENV.PORT || 5000;
 
-const __dirname = path.resolve
+const __dirname = path.resolve();
 
+app.use(express.json());
 app.use('/api', router);
 
-if (ENV.NODE_ENV === 'production')  {
-  app.use(express.static(path.join(__dirname, '../client/dist')))
+if (ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('/{*any}', (req: Request, res: Response) => {
+  app.get('[*any]', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  }) 
+  });
 }
 
-app.listen(PORT, () => {
-  console.log('Server running on port:', PORT);
-});
+(async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log('Server running on port:', PORT);
+  });
+})();
